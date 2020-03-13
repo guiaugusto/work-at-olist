15b60fa1d6936c3790261920bb41d8fd6ecea3ee
Guilherme Augusto <guilherme.francais@gmail.com>
2020-03-13 03:00:25 -0300

    Creates serializer to books endpoint

---
+++ books/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from books.models import Book
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            'id',
+            'name',
+            'edition',
+            'publication_year',
+            'authors',
+            'url'
+        )
