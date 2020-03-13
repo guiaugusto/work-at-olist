8ad9a2405bdfa42d18d301e5c271f9d0cbeb291a
Guilherme Augusto <guilherme.francais@gmail.com>
2020-03-12 23:12:47 -0300

    Creates integration with postgresql in Docker environment

---
+++ scripts/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exporting all environment variables to use in crontab
+env | sed 's/^\(.*\)$/ \1/g' > /root/env
+
+function_postgres_ready() {
+python << END
+import socket
+import time
+import os
+
+port = int(os.environ["POSTGRES_PORT"])
+host = os.environ["POSTGRES_HOST"]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('libstore-db', port))
+s.close()
+END
+}
+
+until function_postgres_ready; do
+  >&2 echo "======= POSTGRES IS UNAVAILABLE, WAITING"
+  sleep 1
+done
+echo "======= POSTGRES IS UP, CONNECTING"
+
+echo '======= MAKING MIGRATIONS'
+python3 manage.py makemigrations
+
+echo '======= RUNNING MIGRATIONS'
+python3 manage.py migrate
+
+echo '======= RUNNING SERVER'
+python3 manage.py runserver 0.0.0.0:4000
